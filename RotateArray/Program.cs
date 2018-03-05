8167b1fd6c1324c4a6d4aa8522bc0f28480a3828
abrahamlaria <abraham@abrahamlaria.com>
2018-03-05 11:24:22 -0800

    Solved using extra array

--- RotateArray/Program.cs
+++ RotateArray/Program.cs
@@ -8,8 +8,34 @@ namespace RotateArray
 {
     class Program
     {
+        public static void LeftRotate(int[] array, int items, int size)
+        {
+            int[] temp = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+               temp[i] = array[(i + items) % size];
+            }
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = temp[i];               
+            }            
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (var t in array)
+            {
+                Console.Write(t + " ");
+            }
+        }
         static void Main(string[] args)
         {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7};
+            int size = 7;
+            int items = 2;
+            LeftRotate(array, items, size);
+            PrintArray(array);
+            Console.ReadLine();
         }
     }
 }
